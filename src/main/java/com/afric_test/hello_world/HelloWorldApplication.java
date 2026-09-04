d7e044dc1d8fbef05dfaf03da4acb9cc1a81338d
unknown <shimiengamaleonelcabrel@gmail.com>
2026-09-04 11:45:30 +0100

    first commit

---
+++ src/main/java/com/afric_test/hello_world/HelloWorldApplication.java
@@ -0,0 +1,26 @@
+package com.afric_test.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from @fric DevOps Pipeline! ✅";
+	}
+
+	@GetMapping("/health")
+	public String health() {
+		return "UP";
+	}
+
+}
