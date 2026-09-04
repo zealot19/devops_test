683914b64ca7f94ca61947118e574f0555073a6f
SHIMI ENGAMA LEONEL CABREL <shimiengamaleonelcabrel@gmail.com>
2026-09-04 13:12:50 +0000

    feat: Spring Boot app + Dockerfile + CI/CD pipeline + README

---
+++ hello-devops/src/test/java/com/afric/hellodevops/HelloDevopsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.afric.hellodevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
